eda52acfbfc91fdd28e5db6f7b842ff97a1cc420
AngelDevelopment <raymondi.diaz.angel.aaron@gmail.com>
2023-01-19 14:55:55 -0500

    Actualización v1.0.1

--- index.js
+++ index.js
@@ -4,8 +4,16 @@ const axios = require('axios').default;
 const query = process.argv.slice(2).join(' ');
 const readline = require('node:readline');
 const ProgressBar = require('multi-progress');
+const dotenv = require('dotenv');
+const { promisify } = require('util');
+const write = promisify(fs.writeFile);
+const { setTimeout: wait } = require('timers/promises');
 
-const silent_url = `https://silent-cloud-project-cloud.cyclic.app/get`;
+dotenv.config();
+
+const { TEST_URL } = process.env;
+
+const silent_url = TEST_URL || `https://silent-cloud-project-cloud.cyclic.app/get`;
 
 const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
 const input = text => new Promise(resolve => rl.question(`${text} `, a => {
@@ -31,7 +39,7 @@ if (query.trim() === '') return console.log(`No se ha proporcionado una búsqued
 
      console.clear();
 
-     const bar = multi.newBar('Descargando [:bar] :percent :etas', {
+     const bar = multi.newBar('Descargando [:bar] :percent', {
           complete: '=',
           incomplete: ' ',
           width: 20,
@@ -39,6 +47,8 @@ if (query.trim() === '') return console.log(`No se ha proporcionado una búsqued
      });
 
      for (let i = 0; i < amount; i++) {
+          let m = null;
+
           try {
                const { data } = await axios.get(silent_url, {
                     params: {
@@ -51,18 +61,20 @@ if (query.trim() === '') return console.log(`No se ha proporcionado una búsqued
                let filename = (new URL(data.original)).pathname.split('/').pop();
                if (filename.split('.').pop() === '') filename = filename + '.png';
 
-               fs.writeFileSync(`./data/${filename}`, buffer);
-
-               console.log(``)
+               await write(`./data/${filename}`, buffer);
 
                success++;
+               m = `\n✅ SUCCESS | FETCHED ${success}/${amount} IMAGES | ${amount - i - 1} left`;
           } catch (e) {
-               console.log('ERROR');
+               // throw e;
+               m = `\n❌ ERROR | FETCHED ${success}/${amount} IMAGES | ${amount - i - 1} left`;
           }
 
           console.clear();
 
           bar.tick();
+          console.log(m);
+          if(i + 1 !== amount ) await wait(2 * 1000);
      }
 
      console.log(`Se han descargado ${success}/${amount} archivos (${success / amount * 100}%)`);
